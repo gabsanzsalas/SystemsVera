31afa95e8a8eea822d2b7019249f4fe2a922b96d
Dotsch17 <47028412+Dotsch17@users.noreply.github.com>
2020-01-30 16:44:34 +0100

    Commit devices

---
+++ AgentV/DeviceDTO.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgentV
+{
+    public class State
+    {
+        public string service { get; set; }
+        public string variable { get; set; }
+        public string value { get; set; }
+        public int id { get; set; }
+        public string pnp { get; set; }
+    }
+
+    public class Service1
+    {
+        public string service { get; set; }
+        public string ControlURL { get; set; }
+        public string EventURL { get; set; }
+        public string serviceType { get; set; }
+    }
+
+    public class Service2
+    {
+        public string service { get; set; }
+        public string ControlURL { get; set; }
+        public string EventURL { get; set; }
+        public string serviceType { get; set; }
+    }
+
+    public class Service3
+    {
+        public string service { get; set; }
+        public string ControlURL { get; set; }
+        public string EventURL { get; set; }
+        public string serviceType { get; set; }
+    }
+
+    public class Service4
+    {
+        public string service { get; set; }
+        public string ControlURL { get; set; }
+        public string EventURL { get; set; }
+        public string serviceType { get; set; }
+    }
+
+    public class Service5
+    {
+        public string service { get; set; }
+        public string ControlURL { get; set; }
+        public string EventURL { get; set; }
+        public string serviceType { get; set; }
+    }
+
+    public class Service7
+    {
+        public string service { get; set; }
+        public string ControlURL { get; set; }
+        public string EventURL { get; set; }
+        public string serviceType { get; set; }
+    }
+
+    public class Service8
+    {
+        public string service { get; set; }
+        public string ControlURL { get; set; }
+        public string EventURL { get; set; }
+        public string serviceType { get; set; }
+    }
+
+    public class Service9
+    {
+        public string service { get; set; }
+        public string ControlURL { get; set; }
+        public string EventURL { get; set; }
+        public string serviceType { get; set; }
+    }
+
+    public class Service10
+    {
+        public string service { get; set; }
+        public string ControlURL { get; set; }
+        public string EventURL { get; set; }
+        public string serviceType { get; set; }
+    }
+
+    public class Service11
+    {
+        public string service { get; set; }
+        public string ControlURL { get; set; }
+        public string EventURL { get; set; }
+        public string serviceType { get; set; }
+    }
+
+    public class Service12
+    {
+        public string service { get; set; }
+        public string ControlURL { get; set; }
+        public string EventURL { get; set; }
+        public string serviceType { get; set; }
+    }
+
+    public class Service13
+    {
+        public string service { get; set; }
+        public string ControlURL { get; set; }
+        public string EventURL { get; set; }
+        public string serviceType { get; set; }
+    }
+
+    public class Service14
+    {
+        public string service { get; set; }
+        public string ControlURL { get; set; }
+        public string EventURL { get; set; }
+        public string serviceType { get; set; }
+    }
+
+    public class Service15
+    {
+        public string service { get; set; }
+        public string ControlURL { get; set; }
+        public string EventURL { get; set; }
+        public string serviceType { get; set; }
+    }
+
+    public class Service16
+    {
+        public string service { get; set; }
+        public string ControlURL { get; set; }
+        public string EventURL { get; set; }
+        public string serviceType { get; set; }
+    }
+
+    public class Service17
+    {
+        public string service { get; set; }
+        public string ControlURL { get; set; }
+        public string EventURL { get; set; }
+        public string serviceType { get; set; }
+    }
+
+    public class Service18
+    {
+        public string service { get; set; }
+        public string ControlURL { get; set; }
+        public string EventURL { get; set; }
+        public string serviceType { get; set; }
+    }
+
+    public class Service19
+    {
+        public string service { get; set; }
+        public string ControlURL { get; set; }
+        public string EventURL { get; set; }
+        public string serviceType { get; set; }
+    }
+
+    public class Service20
+    {
+        public string service { get; set; }
+        public string ControlURL { get; set; }
+        public string EventURL { get; set; }
+        public string serviceType { get; set; }
+    }
+
+    public class Service21
+    {
+        public string service { get; set; }
+        public string ControlURL { get; set; }
+        public string EventURL { get; set; }
+        public string serviceType { get; set; }
+    }
+
+    public class Service22
+    {
+        public string service { get; set; }
+        public string ControlURL { get; set; }
+        public string EventURL { get; set; }
+        public string serviceType { get; set; }
+    }
+
+    public class Service23
+    {
+        public string service { get; set; }
+        public string ControlURL { get; set; }
+        public string EventURL { get; set; }
+        public string serviceType { get; set; }
+    }
+
+    public class Service24
+    {
+        public string service { get; set; }
+        public string ControlURL { get; set; }
+        public string EventURL { get; set; }
+        public string serviceType { get; set; }
+    }
+
+    public class Service25
+    {
+        public string service { get; set; }
+        public string ControlURL { get; set; }
+        public string EventURL { get; set; }
+        public string serviceType { get; set; }
+    }
+
+    public class Service26
+    {
+        public string service { get; set; }
+        public string ControlURL { get; set; }
+        public string EventURL { get; set; }
+        public string serviceType { get; set; }
+    }
+
+    public class Service27
+    {
+        public string service { get; set; }
+        public string ControlURL { get; set; }
+        public string EventURL { get; set; }
+        public string serviceType { get; set; }
+    }
+
+    public class ControlURLs
+    {
+        public Service1 service_1 { get; set; }
+        public Service2 service_2 { get; set; }
+        public Service3 service_3 { get; set; }
+        public Service4 service_4 { get; set; }
+        public Service5 service_5 { get; set; }
+        public Service7 service_7 { get; set; }
+        public Service8 service_8 { get; set; }
+        public Service9 service_9 { get; set; }
+        public Service10 service_10 { get; set; }
+        public Service11 service_11 { get; set; }
+        public Service12 service_12 { get; set; }
+        public Service13 service_13 { get; set; }
+        public Service14 service_14 { get; set; }
+        public Service15 service_15 { get; set; }
+        public Service16 service_16 { get; set; }
+        public Service17 service_17 { get; set; }
+        public Service18 service_18 { get; set; }
+        public Service19 service_19 { get; set; }
+        public Service20 service_20 { get; set; }
+        public Service21 service_21 { get; set; }
+        public Service22 service_22 { get; set; }
+        public Service23 service_23 { get; set; }
+        public Service24 service_24 { get; set; }
+        public Service25 service_25 { get; set; }
+        public Service26 service_26 { get; set; }
+        public Service27 service_27 { get; set; }
+    }
+
+    public class Wakeup
+    {
+        public List<int> list { get; set; }
+        public int lastKnown { get; set; }
+    }
+
+    public class Device
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string device_type { get; set; }
+        public string device_file { get; set; }
+        public List<State> states { get; set; }
+        public ControlURLs ControlURLs { get; set; }
+        public object category_num { get; set; }
+        public object subcategory_num { get; set; }
+        public object time_created { get; set; }
+        public string invisible { get; set; }
+        public string local_udn { get; set; }
+        public object room { get; set; }
+        public int? id_parent { get; set; }
+        public object embedded { get; set; }
+        public int? disabled { get; set; }
+        public string device_json { get; set; }
+        public string impl_file { get; set; }
+        public string manufacturer { get; set; }
+        public string model { get; set; }
+        public string altid { get; set; }
+        public string ip { get; set; }
+        public string mac { get; set; }
+        public string plugin { get; set; }
+        public string pnp { get; set; }
+        public List<string> poll { get; set; }
+        public Wakeup wakeup { get; set; }
+        public string onDashboard { get; set; }
+        public string no_child_sync { get; set; }
+    }
+}
--- AgentV/Worker.cs
+++ AgentV/Worker.cs
@@ -5,7 +5,10 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Net.Http;
 using MQTTnet;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Microsoft.EntityFrameworkCore;
 
 namespace AgentV
@@ -15,20 +18,51 @@ namespace AgentV
         private readonly ILogger<Worker> _logger;
         MqttApplicationMessage message;
         DbContext dbContext;
+        protected readonly HttpClient _httpClient;
+        protected Uri BaseEndpoint { get; set; }
 
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
-            
+            _httpClient = new HttpClient();
+            BaseEndpoint = new Uri("http://10.0.2.134:3480/data_request?id=user_data");
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
+            //while (!stoppingToken.IsCancellationRequested)
             {
+                string devices = Get<string>();
+                Console.WriteLine(devices);
+                Device[] devicess = Get<Device[]>();
+                Console.WriteLine(JsonConvert.SerializeObject(devicess));
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(5000, stoppingToken);
+                await Task.Delay(10000, stoppingToken);
             }
+        }
+
+        /// <summary>  
+        /// Common method for making GET calls  
+        /// </summary>  
+        protected T Get<T>()
+        {
+
+            HttpResponseMessage response = _httpClient.GetAsync(BaseEndpoint, HttpCompletionOption.ResponseHeadersRead).Result;
+            var data = response.Content.ReadAsStringAsync().Result;
+
+            if (!response.IsSuccessStatusCode)
+                throw new Exception(data);
+
+            return ConvertResult<T>(data);
+        }
+
+        private T ConvertResult<T>(string data)
+        {
+            data = JObject.Parse(data)["devices"].ToString();
+            if (typeof(T) == typeof(string))
+                return (T)Convert.ChangeType(data, typeof(T));
+            else
+                return JsonConvert.DeserializeObject<T>(data);
         }
     }
 }
