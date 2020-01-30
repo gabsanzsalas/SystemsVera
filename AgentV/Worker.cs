2268018305cbd94c08053f0b6b109482ff539090
Manuel Ladurner <manuel.ladurner02@gmail.com>
2020-01-30 16:34:55 +0100

    Added mqttclient

--- AgentV/Worker.cs
+++ AgentV/Worker.cs
@@ -1,11 +1,28 @@
+using MQTTnet; //LADI
+
+using MQTTnet.Client;//LADI
+
+using Microsoft.Extensions.DependencyInjection;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Connecting;
+using MQTTnet.Client.Options;
+using MQTTnet.Client.Publishing;
+using MQTTnet.Client.Subscribing;
+using MQTTnet.Protocol;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Dynamic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using MQTTnet;
 using Microsoft.EntityFrameworkCore;
 
 namespace AgentV
@@ -15,7 +32,10 @@ namespace AgentV
         private readonly ILogger<Worker> _logger;
         MqttApplicationMessage message;
         DbContext dbContext;
-
+        private IMqttClient mqttClient;// = new IMqttClient("camotemqtt.westeurope.azurecontainer.io", "veraplus",
+                                       //"A2uhXG4GLOGfHp47daVA", 2, "CLIENTID"); //LADI
+        private MqttFactory factory = new MqttFactory();//LADI
+       
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
@@ -24,11 +44,39 @@ namespace AgentV
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            mqttClient = factory.CreateMqttClient();
+            MqttClientAuthenticateResult result = await mqttClient.ConnectAsync(SetConnectionOptions(false, "1234", "camotemqtt.westeurope.azurecontainer.io", 1883, "veraplus", "A2uhXG4GLOGfHp47daVA"), CancellationToken.None);//cleanSession
             while (!stoppingToken.IsCancellationRequested)
             {
+                Publish("45fea213","ABCD");
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 await Task.Delay(5000, stoppingToken);
             }
+        }
+
+        private IMqttClientOptions SetConnectionOptions(bool cleanSession,
+            String clientId, String url, int port, String user, String password)
+        {
+            var options = new MqttClientOptionsBuilder()
+            .WithClientId(clientId)
+            .WithTcpServer(url, port)
+            .WithCredentials(user, password)
+            .WithCleanSession(cleanSession)
+            .Build();
+
+            return options;
+        }//LADI
+
+        public async void Publish(string topic, string payload)////
+        {
+             MqttApplicationMessage message = new MqttApplicationMessageBuilder()
+                .WithTopic(topic)
+                .WithPayload(payload)
+                .WithQualityOfServiceLevel(MQTTnet.Protocol.MqttQualityOfServiceLevel.AtLeastOnce)
+                .WithRetainFlag(false)
+                .Build();
+
+                await mqttClient.PublishAsync(message, CancellationToken.None);
         }
     }
 }
